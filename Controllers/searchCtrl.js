42e2669e625db9ea820bcc16865facd98a0df802
anewandrew <92andrew.ha@gmail.com>
2015-07-06 18:01:28 -0400

    gets gif data

--- Controllers/searchCtrl.js
+++ Controllers/searchCtrl.js
@@ -1,10 +1,21 @@
 function SearchCtrl(ApiService) {
 	var self = this;
-	this.service = ApiService;
-	ApiService.getTags().then(function(response) {
+	this.apiService = ApiService;
+	self.gifData
+
+
+	this.apiService.getTags().then(function(response) {
 		console.log(response);
 		self.tags = response.data;
 	})
 }
-
+SearchCtrl.prototype.onSelect = function ($item, $model, $label) {
+	var self = this;
+    this.apiService.getGifData($item).then(function(data) {
+    	self.gifData = data.data.gifs;
+    	console.log(self.gifData);
+    }).catch(function(data) {
+    	console.log(data);
+    });
+};
 angular.module('gifApp').controller('SearchCtrl', SearchCtrl);
--- services/ApiService.js
+++ services/ApiService.js
@@ -1,10 +1,14 @@
 function ApiService($http) {
 	var self = this;
 	this.$http = $http;
-
 }
 ApiService.prototype.getTags = function() {
 	return this.$http.get('http://www.gifbase.com/js/tags.json', {})
 	}
+	
+ApiService.prototype.getGifData = function(tag) {
+	return this.$http.get('http://www.gifbase.com/tag/'+tag+'?format=json', {})
+}
+
 
 angular.module('gifApp').service('ApiService', ApiService);
