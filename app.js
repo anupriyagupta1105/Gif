8df5f1879850dcf36bfd5a4ab26676ed306581cc
anupriyagupta1105 <anupriyagupta1105@gmail.com>
2015-07-06 15:30:32 -0400

    more controllers

---
+++ app.js
@@ -0,0 +1,14 @@
+var app= angular.module('GifApp' , ['ngRoute']);
+
+app.config(function($routeProvidder){
+	$routeProvider
+		.when('/search',{
+			templateUrl: 'templates/search.html',
+			controller: 'searchCtrl as ctrl'
+		})
+		.when('/details',{
+			templateUrl: 'templates/details.html',
+			controller: 'detailsCtrl as ctrl'
+		})
+}
+
