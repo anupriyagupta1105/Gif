ff2d7304c27dab1b7eb78696947392ec4b257e65
anupriyagupta1105 <anupriyagupta1105@gmail.com>
2015-07-06 14:54:03 -0400

    added services

---
+++ services/ApiService.js
@@ -0,0 +1,18 @@
+
+$.ajax({
+  url:'http://www.gifbase.com/js/tags.JSON',
+  type:'GET',
+  data:{},
+})
+.then(function(data){
+  //success promise
+  console.log(data);
+},
+function(data){
+  //failure promise
+  console.log(data);
+})
+.always(function(data){
+  //code to run whether function is success or error
+});
+;
